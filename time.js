4d77e68335ed3e28375753a2f23e3dc4adc7bee9
Marco-coder8 <m3rcodiaz@gmail.com>
2026-02-27 04:20:33 +0200

    Add files via upload

---
+++ time.js
@@ -0,0 +1,24 @@
+const targetDate = new Date("Jan 1, 2027 00:00:00").getTime();
+
+function updateTimer() {
+    const now = new Date().getTime();
+    const gap = targetDate - now;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const d = Math.floor(gap / day);
+    const h = Math.floor((gap % day) / hour);
+    const m = Math.floor((gap % hour) / minute);
+    const s = Math.floor((gap % minute) / second);
+
+    document.getElementById("days").innerText = d;
+    document.getElementById("hours").innerText = h.toString().padStart(2, '0');
+    document.getElementById("minutes").innerText = m.toString().padStart(2, '0');
+    document.getElementById("seconds").innerText = s.toString().padStart(2, '0');
+}
+
+setInterval(updateTimer, 1000);
+updateTimer();
